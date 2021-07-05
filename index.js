df089d4fa20614da80ca92ed5e709da03da886c6
kalpana1 <dkdevi.dkd@gmail.com>
2021-07-05 18:05:41 +0530

    grabbed all elements

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+current_display = document.querySelector('.current');
+previous_display = document.querySelector('.previous');
+
+number = document.querySelectorAll('.number');
+operation = document.querySelectorAll('.operation');
+
+clear = document.querySelector('.clear');
+clear_lastone = document.querySelector('.clear_last');
+equal = document.querySelector('.equal');
