12fb158f7fbd404f2bcc9f394ab1585aad9b8538
kalpana1 <dkdevi.dkd@gmail.com>
2021-07-05 18:34:59 +0530

    display number

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
+class Calculator {
+    constructor(current_display, previous_display) {
+        this.current_display = current_display;
+        this.previous_display = previous_display;
+        this.currentNumber = '';
+    }
+    clear() {
 
+    }
+    clear_last_entered() {
 
+    }
+    append_Number(incoming_number) {
+        this.currentNumber += incoming_number;
 
+    }
+    chooseOperation() {
 
+    }
+    compute() {
 
+    }
+    display() {
+        this.current_display.innerHTML = this.currentNumber;
+        //this.previous_display.innerHTML = this.previousNumber;
+    }
+}
 
 
 current_display = document.querySelector('.current');
@@ -14,3 +36,13 @@ operation = document.querySelectorAll('.operation');
 clear = document.querySelector('.clear');
 clear_lastone = document.querySelector('.clear_last');
 equal = document.querySelector('.equal');
+
+calculator = new Calculator(current_display, previous_display)
+
+number.forEach(num => {
+    num.addEventListener('click', (e) => {
+        calculator.append_Number(e.target.innerHTML);
+        calculator.display();
+    })
+
+});
